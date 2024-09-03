d75245812731f2e0eed718e0c3a9e9e336bba589
Mario Winter <mariowinter.sg@gmail.com>
2024-09-03 22:09:05 +0200

    feat: add Angular Material components to dialog-add-user component

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
-  selector: 'app-dialog-add-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+	selector: "app-dialog-add-user",
+	standalone: true,
+	imports: [MatDialogContent, MatFormFieldModule, MatDialogActions, MatButtonModule, FormsModule, MatInputModule, MatDialogTitle, MatDialogClose],
+	templateUrl: "./dialog-add-user.component.html",
+	styleUrl: "./dialog-add-user.component.scss",
 })
-export class DialogAddUserComponent {
-
-}
+export class DialogAddUserComponent {}
