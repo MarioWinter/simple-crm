7541a96b52fc6a298e3acdd57972e3636b980693
Mario Winter <mariowinter.sg@gmail.com>
2024-09-03 22:35:52 +0200

    feat(user): enhance add-user dialog with form fields and styling improvements

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,15 +1,30 @@
 import { Component } from "@angular/core";
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from "@angular/material/dialog";
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
 
 @Component({
 	selector: "app-dialog-add-user",
 	standalone: true,
-	imports: [MatDialogContent, MatFormFieldModule, MatDialogActions, MatButtonModule, FormsModule, MatInputModule, MatDialogTitle, MatDialogClose],
+	imports: [
+		MatDialogContent,
+		MatFormFieldModule,
+		MatDialogActions,
+		MatButtonModule,
+		FormsModule,
+		MatInputModule,
+		MatDialogTitle,
+		MatDialogClose,
+		MatIconModule,
+		MatDividerModule,
+	],
 	templateUrl: "./dialog-add-user.component.html",
 	styleUrl: "./dialog-add-user.component.scss",
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+	constructor(public dialog: MatDialog) {}
+}
