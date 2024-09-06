521f609c636299d259ecfdfe7226800297090deb
Mario Winter <mariowinter.sg@gmail.com>
2024-09-06 20:04:27 +0200

    `feat(firebase): integrate AngularFire and connect project to Simple-CRM`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { GermanDateAdapter } from "./core/services/german-date-adapter.service";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 const DE_DATE_FORMATS = {
 	parse: {
@@ -24,6 +26,6 @@ export const appConfig: ApplicationConfig = {
 		provideAnimationsAsync(),
 		{ provide: MAT_DATE_LOCALE, useValue: "de-DE" },
 		{ provide: DateAdapter, useClass: GermanDateAdapter },
-		{ provide: MAT_DATE_FORMATS, useValue: DE_DATE_FORMATS },
+		{ provide: MAT_DATE_FORMATS, useValue: DE_DATE_FORMATS }, provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-d8c8d","appId":"1:444629331602:web:38a523f158dcfccd44804b","storageBucket":"simple-crm-d8c8d.appspot.com","apiKey":"AIzaSyDb_Q_cFrFjYv6pKc-5M3LG4sM9UXZ82jg","authDomain":"simple-crm-d8c8d.firebaseapp.com","messagingSenderId":"444629331602"})), provideFirestore(() => getFirestore()),
 	],
 };
