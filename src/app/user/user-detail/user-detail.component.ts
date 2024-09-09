ffff8b25f6ec4d10b252c4a3eea3d8abfed7dce7
Mario Winter <mariowinter.sg@gmail.com>
2024-09-09 21:45:01 +0200

    feat(user-detail): enhance user detail UI with card layout and styling

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatDialog, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatDialogRef } from "@angular/material/dialog";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-user-detail',
-  standalone: true,
-  imports: [],
-  templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+	selector: "app-user-detail",
+	standalone: true,
+	imports: [CommonModule, MatCardModule, MatIconModule],
+	templateUrl: "./user-detail.component.html",
+	styleUrl: "./user-detail.component.scss",
 })
-export class UserDetailComponent {
-
-}
+export class UserDetailComponent {}
